6a5cf3fa9412d2947184750a355de6b018e33b92
Mario Tavarez <mariotavarez@flyzz.info>
2021-10-16 15:37:58 -0500

    INDEX: Corrección de crossorigin

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,6 @@ const Home: NextPage = () => {
       <Head>
         <title>FLYZZ - Control de pedidos</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
-          integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
-        />
       </Head>
       <Login />
     </div>
